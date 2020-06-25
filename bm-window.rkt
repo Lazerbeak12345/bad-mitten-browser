f0a8f6bfbe813a424c9b4a8c378772d34fb53cf5
Nathan Fritzler <nfblaster@live.com>
2020-06-24 20:05:00 -0600

    Convert the window file into an object instead of a function

--- bm-window.rkt
+++ bm-window.rkt
@@ -9,45 +9,46 @@
 
 (require "tab.rkt")
 
-(define/contract
-  (bm-window tab-links) ((or/c (listof string?)
-							   string?
-							   )
-						 . -> .
-						 void?
-						 )
-  (define frame (new frame%
-					 [label "Bad-Mitten Browser"]
-					 [width 800] ; I just guessed these numbers. Works for gnome, works for me
-					 [height 600]
-					 )
-	)
-  (define locationPanel (new horizontal-panel%
-							 [parent frame]
-							 [alignment '(left top)]
-							 )
-	)
-  ; The location box. I would prefer if this were in the top bar instead.
-  (define locationBox (new text-field%
-						   [parent locationPanel]
-						   [label "URL:"]
-						   )
-	)
-  (define tabs (for/list ([tab-link (if (list? tab-links)
-									  tab-links
-									  (list tab-links)
-									  )
-									]
-						  )
-				 (new tab%
-					  [url tab-link]
-					  [parent frame]
-					  [locationBox locationBox]
+(define bm-window% (class object% (init links) ;TODO use signatures
+					(define self-links (cond [((listof string?) links) links]
+											 [(string? links) (list links)]
+										 )
 					  )
-				 )
-	)
-  (send (first tabs) focus)
-  (send frame show #t)
+					(define frame (new frame%
+									   [label "Bad-Mitten Browser"]
+									   [width 800] ; I just guessed these numbers. Works for gnome, works for me
+									   [height 600]
+									   )
+					  )
+					(define locationPanel (new horizontal-panel%
+											   [parent frame]
+											   [alignment '(left top)]
+											   )
+					  )
+					; The location box. I would prefer if this were in the top bar instead.
+					(define locationBox (new text-field%
+											 [parent locationPanel]
+											 [label "URL:"]
+											 )
+					  )
+					(define tabs (for/list ([tab-link self-links])
+								   (new tab%
+										[url tab-link]
+										[parent frame]
+										[locationBox locationBox]
+										)
+								   )
+					  )
+					(super-new)
+					(send (first tabs) focus)
+					(send frame show #t)
+					(define/public (get-locationBox)
+					  locationBox
+					  )
+					(define/public (get-tabs)
+					  tabs
+					  )
+					)
   )
-(provide bm-window)
+(provide bm-window%)
 
--- index.rkt
+++ index.rkt
@@ -4,6 +4,6 @@
 
 (print-warning "Opening test.html, as I don't know how to do command line args")
 (require "bm-window.rkt")
-(bm-window "test.html")
-;(bm-window '("test.html" "test.html"))
+(new bm-window% [links "test.html"])
+;(bm-window% [links '("test.html" "test.html")])
 
