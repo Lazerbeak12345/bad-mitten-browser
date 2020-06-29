701c83f38e735f7e244ab576cf683ffe77baf4cb
Nathan Fritzler <nfblaster@live.com>
2020-06-28 23:12:31 -0600

    Notify user of some http errors

--- networking.rkt
+++ networking.rkt
@@ -13,15 +13,28 @@
   (case (url-scheme theUrl)
 	[("file") (getTreeFromPortAndCloseIt (open-input-file (url->path theUrl)))]
 	;TODO is this the proper way to do https?
-	[("http" "https") (let-values ([(port headers) (get-pure-port/headers theUrl
-																		  #:connection (make-http-connection)
-																		  )
-												   ]
-								   )
-						(print-info headers)
-						(getTreeFromPortAndCloseIt port)
-						)
-					  ]
+	[("http" "https")
+	 (with-handlers ([exn:fail:network:errno? (lambda (e)
+												`(*TOP* (body (@ (style "height:100%"))
+															  (strong (@ (style "margin:auto;"))
+																	  ,(exn-message e)
+																	  )
+															  )
+														)
+												)
+											  ]
+					 )
+					(let-values ([(port headers)
+								  (get-pure-port/headers theUrl
+														 #:connection (make-http-connection)
+														 )
+								  ]
+								 )
+					  (print-info headers)
+					  (getTreeFromPortAndCloseIt port)
+					  )
+					)
+	 ]
 	[(#f) (print-error (string-append "Can't handle a lack of a scheme")) null]
 	[else (print-error (string-append "Can't handle this scheme "
 									  (url-scheme theUrl)
