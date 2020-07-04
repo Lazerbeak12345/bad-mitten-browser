0f72850a3088ab0e00982b65c9e819fea83ae3e5
Nathan Fritzler <nfblaster@live.com>
2020-07-03 22:41:27 -0600

    Add bm:\\ not found page along with contracts

--- pages.rkt
+++ pages.rkt
@@ -3,7 +3,8 @@
 (provide bmUrl
          makeErrorMessage
          )
-(define (makeErrorMessage e)
+(define/contract 
+  (makeErrorMessage e) (string? . -> . list?)
   `(*TOP* (html (body (@ (style "height:100%"))
                       (strong (@ (style "margin:auto;"))
                               ,e
@@ -12,26 +13,29 @@
                 )
           )
   )
-(define (bmUrl theUrl)
-  (define paths (if (not (url-host theUrl))
-                  (for/list [(path (url-path theUrl))]
-                    (path/param-path path)
-                    )
-                  (cons (url-host theUrl)
-                        (for/list [(path (url-path theUrl))]
-                          (path/param-path path)
-                          )
-                        )
-                  )
-    )
-  (case (first paths)
-    [("newtab")
-     '(*TOP* (*DECL* DOCTYPE html)
-             (html (head (title "New Tab"))
-                   (body (h1 "Bad Mitten" (i "Browser")))
+(define/contract (bmUrl theUrl) (url? . -> . list?)
+                 (define/contract 
+                   paths (listof (or/c string? 'up 'same))
+                   (if (not (url-host theUrl))
+                     (for/list [(path (url-path theUrl))]
+                       (path/param-path path)
+                       )
+                     (cons (url-host theUrl)
+                           (for/list [(path (url-path theUrl))]
+                             (path/param-path path)
+                             )
+                           )
+                     )
                    )
-             )
-     ]
-    )
-  )
+                 (case (first paths)
+                   [("newtab")
+                    '(*TOP* (*DECL* DOCTYPE html)
+                            (html (head (title "New Tab"))
+                                  (body (h1 "Bad Mitten" (i "Browser")))
+                                  )
+                            )
+                    ]
+                   [else (makeErrorMessage "Page does not exist on 'bm:' url")]
+                   )
+                 )
 
