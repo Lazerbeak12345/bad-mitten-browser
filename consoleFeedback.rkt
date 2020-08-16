1c9d8879012ac152007d64730c2baa8de6080132
Nathan Fritzler <nfblaster@live.com>
2020-08-15 22:04:38 -0600

    Start conversion into typed racket where possible

--- consoleFeedback.rkt
+++ consoleFeedback.rkt
@@ -1,64 +1,59 @@
-#lang racket/base
-(require racket/date racket/contract)
+#lang typed/racket/base
+(require typed/racket/date)
 (provide print-info
          print-warning
          print-error
          get-verbosity
          set-verbosity!
-         verbosity-level?)
+         VerbosityLevel)
 ; Print information, warnings, and the like to the console that this was run
 ; from
 
-(define/contract verbosity-level? contract?
-                 (or/c 'all 'errors 'warnings 'errors-and-warnings 'none))
-(define/contract verbosity verbosity-level? 'all)
-(define/contract (get-verbosity) (-> verbosity-level?) verbosity)
-(define/contract (set-verbosity! new-verbosity) (-> verbosity-level? void?)
-                 (unless (eq? verbosity new-verbosity)
-                   (let ([info-before
-                           (format "Verbosity changing from ~a to ~a"
-                                   verbosity
-                                   new-verbosity)]
-                         [info-after (format "Verbosity changed from ~a to ~a"
-                                             verbosity
-                                             new-verbosity)])
-                     ; NOTE I am printing it before and after so one can tell
-                     ; for certian when what change happened if it was to or
-                     ; from all mode.
-                     (print-warning info-before)
-                     (set! verbosity new-verbosity)
-                     (print-warning info-after))))
+(define-type VerbosityLevel
+             (U 'all 'errors 'warnings 'errors-and-warnings 'none))
+(: verbosity VerbosityLevel)
+(define verbosity 'all)
+(: get-verbosity (-> VerbosityLevel))
+(define (get-verbosity) verbosity)
+(: set-verbosity! (-> VerbosityLevel Void))
+(define (set-verbosity! new-verbosity)
+  (unless (eq? verbosity new-verbosity)
+    (let ([info-before (format "Verbosity changing from ~a to ~a"
+                               verbosity
+                               new-verbosity)]
+          [info-after (format "Verbosity changed from ~a to ~a"
+                              verbosity
+                              new-verbosity)])
+      ; NOTE I am printing it before and after so one can tell for certian when
+      ; what change happened if it was to or from all mode.
+      (print-warning info-before)
+      (set! verbosity new-verbosity)
+      (print-warning info-after)
+      )))
 
 (define (getDisplayTime)
   (date-display-format 'iso-8601) 
   (date->string (current-date) #t))
 
-(define/contract 
-  (print-info information)
-  (string? . -> . void?)
-  (when (eq? verbosity 'all)
-    (displayln (format "[~a] INFO:    ~a"
-                       (getDisplayTime)
-                       information))))
+(define-type PrintThingy (-> String Void))
 
-(define/contract 
-  (print-warning information)
-  (string? . -> . void?)
+(: print-info PrintThingy)
+(define (print-info information)
+  (when (eq? verbosity 'all)
+    (displayln (format "[~a] INFO:    ~a" (getDisplayTime) information))))
+
+(: print-warning PrintThingy)
+(define (print-warning information)
   (when (or (eq? verbosity 'all)
             (eq? verbosity 'warnings)
             (eq? verbosity 'errors-and-warnings))
-    (displayln (format "[~a] WARNING: ~a"
-                       (getDisplayTime)
-                       information))))
+    (displayln (format "[~a] WARNING: ~a" (getDisplayTime) information))))
 
-(define/contract 
-  (print-error information)
-  (string? . -> . void?)
+(: print-error PrintThingy)
+(define (print-error information)
   (when (or (eq? verbosity 'all)
             (eq? verbosity 'errors)
             (eq? verbosity 'errors-and-warnings))
-    (displayln (format "[~a] ERROR:   ~a"
-                       (getDisplayTime)
-                       information)
+    (displayln (format "[~a] ERROR:   ~a" (getDisplayTime) information)
                (current-error-port))))
 (print-info (format "Verbosity level is currently ~a" verbosity))
