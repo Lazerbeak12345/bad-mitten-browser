2e4941f55f6bdb3325f8530a01adf6d258dec93b
Nathan Fritzler <nfblaster@live.com>
2020-08-20 17:32:36 -0600

    Convert renderer into typed/racket

--- renderer.rkt
+++ renderer.rkt
@@ -1,6 +1,10 @@
-#lang racket/base
-(require racket/gui/base racket/class "consoleFeedback.rkt")
+#lang typed/racket/base
+(require typed/racket/gui/base typed/racket/class "consoleFeedback.rkt")
 (provide make-canvas)
+(: make-canvas (-> (U (Instance Frame%)
+                      (Instance Panel%)
+                      (Instance Dialog%) 
+                      (Instance Pane%)) (Instance Canvas%)))
 (define (make-canvas parent)
   (new
     canvas%
