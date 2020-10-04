d9272bdf6e03e8f7446472b31cc02112896e4b51
Nathan Fritzler <nfblaster@live.com>
2020-10-04 11:13:14 -0600

    Make renderer% a subclass of editor-canvas%

--- bird-E/renderer.rkt
+++ bird-E/renderer.rkt
@@ -1,13 +1,17 @@
 #lang typed/racket/base
-(require typed/net/url typed/racket/class "../consoleFeedback.rkt")
-(define-type Renderer% (Class (init [initial-URL URL]
-                                    [parent Any]
+(require typed/net/url
+         typed/racket/class
+         typed/racket/gui/base
+         "../consoleFeedback.rkt")
+(define-type Renderer% (Class #:implements/inits Editor-Canvas%
+                              (init [initial-URL URL]
                                     [setUrl! (-> URL Void)])
                               [navigate-to (-> URL Void)]))
 (provide renderer% Renderer%)
 (define renderer% : Renderer%
-  (class object%
-    (init initial-URL parent setUrl!)
+  (class editor-canvas%
+    (init initial-URL setUrl!)
     (define/public (navigate-to theUrl)
       (print-info (format "navigate-to ~a" theUrl)))
+    (init-rest)
     (super-new)))
