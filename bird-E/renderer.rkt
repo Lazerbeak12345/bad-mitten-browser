d1e4fde879a7abffb6be94b2c5107b1889fa4d78
Nathan Fritzler <nfblaster@live.com>
2020-10-04 22:30:41 -0600

    Add a pasteboard

--- bird-E/renderer.rkt
+++ bird-E/renderer.rkt
@@ -3,20 +3,23 @@
          typed/racket/class
          typed/racket/gui/base
          "../consoleFeedback.rkt")
+(provide renderer% Renderer%)
 (define-type Renderer% (Class (init [initial-URL URL]
                                     [setUrl! (-> URL Void)]
                                     [parent (Instance Area-Container<%>)])
                               [navigate-to (-> URL Void)]))
-(provide renderer% Renderer%)
 (define renderer% : Renderer%
   (class object%
     (init initial-URL setUrl! parent)
     (define theUrl : URL initial-URL)
     (define init-setUrl! : (-> URL Void) setUrl!)
     (define init-parent : (Instance Area-Container<%>) parent)
+    (define pasteboard-instance : (Instance Pasteboard%)
+      (new pasteboard%))
     (define editor-canvas-instance : (Instance Editor-Canvas%)
       (new editor-canvas%
            [parent init-parent]
+           [editor pasteboard-instance]
            [style '(auto-vscroll auto-hscroll)]))
     (super-new)
     (define/public (navigate-to theUrl)
