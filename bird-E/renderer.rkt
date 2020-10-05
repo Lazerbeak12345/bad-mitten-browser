1ce6ac3f0823c32fe217e66f498fe96e5b00032b
Nathan Fritzler <nfblaster@live.com>
2020-10-04 22:00:19 -0600

    Move away from allowing access to the implimentation of renderer
    This means it has to keep track of a bit more than I'd like, but I'll
    add that as-needed.

--- bird-E/renderer.rkt
+++ bird-E/renderer.rkt
@@ -3,15 +3,22 @@
          typed/racket/class
          typed/racket/gui/base
          "../consoleFeedback.rkt")
-(define-type Renderer% (Class #:implements/inits Editor-Canvas%
-                              (init [initial-URL URL]
-                                    [setUrl! (-> URL Void)])
+(define-type Renderer% (Class (init [initial-URL URL]
+                                    [setUrl! (-> URL Void)]
+                                    [parent (Instance Area-Container<%>)])
                               [navigate-to (-> URL Void)]))
 (provide renderer% Renderer%)
 (define renderer% : Renderer%
-  (class editor-canvas%
-    (init initial-URL setUrl!)
+  (class object%
+    (init initial-URL setUrl! parent)
+    (define theUrl : URL initial-URL)
+    (define init-setUrl! : (-> URL Void) setUrl!)
+    (define init-parent : (Instance Area-Container<%>) parent)
+    (define editor-canvas-instance : (Instance Editor-Canvas%)
+      (new editor-canvas%
+           [parent init-parent]
+           [style '(auto-vscroll auto-hscroll)]))
+    (super-new)
     (define/public (navigate-to theUrl)
-      (print-info (format "navigate-to ~a" theUrl)))
-    (init-rest)
-    (super-new)))
+      (print-info (format "navigate-to ~a" theUrl))))) 
+
