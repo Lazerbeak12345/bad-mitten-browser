0e6667dd2d412f7c612f0421fabb5f872f6e18f1
Nathan Fritzler <nfblaster@live.com>
2020-12-20 22:57:09 -0700

    Pass doctype down dom tree

--- bird-E/xexp-to-dom.rkt
+++ bird-E/xexp-to-dom.rkt
@@ -9,8 +9,9 @@
 (define (html-br? theXexp)
   (and (xexp? theXexp)
 	   (eq? 'br (xexp-name theXexp))))
-(: xexp->dom (-> (Listof Xexp) (Listof Any))) ; TODO narrow return type
-(define (xexp->dom xexp)
+; NOTE: changes to #:doctype are not propigated upwards through the dom
+(: xexp->dom (-> (Listof Xexp) [#:doctype (U 'html5 'quirks)] (Listof Any))) ; TODO narrow return type
+(define (xexp->dom xexp #:doctype [doctype 'html5])
   (define last-string : String "")
   (define cleaned-elms : (Listof Xexp) null)
   (for ([elm xexp])
@@ -22,10 +23,16 @@
       [(xexp-short? elm)
 	   (set! last-string (string-append (string (xexp-short->char elm))))]
 	  [else
-       (set! cleaned-elms (append cleaned-elms (list (assert last-string xexp?) elm)))
-       (set! last-string "")]))
-  (define doctype 'html5)
-  (for/list ([elm cleaned-elms])
+		(set! cleaned-elms (append cleaned-elms
+								   (if (not (eq? last-string ""))
+									 (list (assert last-string xexp?) elm)
+									 (list elm))))
+		(set! last-string "")]))
+  (for/list ([elm cleaned-elms]
+			 #:when (if (xexp-decl? elm)
+					  (begin (print-error "Needs to update doctype")
+							 #f)
+					  #t))
     (cond
       [(string? elm)
 	   ; As it turns out, this built-in does everything I need.
@@ -36,5 +43,5 @@
 	   (new dom-elm%
 			[name (xexp-name (ann elm Xexp))]
 			[attrs (xexp-attrs elm)]
-			[children (xexp->dom (xexp-children elm))])])))
+			[children (xexp->dom (xexp-children elm) #:doctype doctype)])])))
 
