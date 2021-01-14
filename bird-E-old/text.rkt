802e417fedf446a5f072c4849cde73d224e8e715
Nathan Fritzler <nfblaster@live.com>
2021-01-13 22:02:28 -0700

    Remove the old bird-E files

--- bird-E-old/text.rkt
+++
@@ -1,8 +0,0 @@
-#lang typed/racket/base
-(require typed/racket/class "../consoleFeedback.rkt" "character-data.rkt")
-(provide Text% text%)
-(define-type Text% (Class #:implements/inits Character-Data%))
-(define text%
-  (class character-data%
-    (print-info "text% initted!")
-    (super-new)))
