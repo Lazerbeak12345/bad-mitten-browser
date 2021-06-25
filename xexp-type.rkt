34233a537c916f21a9adfdc3a07b2e32d58e20e9
Nathan Fritzler <nfblaster@live.com>
2021-06-25 00:26:19 -0600

    Update xexp-type.rkt to use `only-in`
    Signed-off-by: Nathan Fritzler <nfblaster@live.com>

--- xexp-type.rkt
+++ xexp-type.rkt
@@ -16,7 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 |#
-(require "consoleFeedback.rkt")
+(require (only-in "consoleFeedback.rkt" print-error))
 (define-type Xexp-decl (Pair '*DECL* (Listof (U String Symbol))))
 (define-predicate xexp-decl? Xexp-decl)
 (provide xexp-decl? Xexp-decl)
